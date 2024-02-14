33e7119877e9e203fd8192b6b789d146d43dd5a9
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-14 15:32:03 +0300

    Menu component'i oluşturuldu.

---
+++ src/components/Sidebar/Menu.jsx
@@ -0,0 +1,49 @@
+import { Icon } from "../../Icons";
+
+function Menu() {
+  return (
+    <nav className="px-2">
+      <ul className="flex flex-col">
+        <li>
+          <a
+            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active"
+            href=""
+          >
+            <span>
+              <Icon name="home" />
+            </span>
+            Anasayfa
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a
+            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4"
+            href=""
+          >
+            <span>
+              <Icon name="search" />
+            </span>
+            Ara
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a
+            className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4"
+            href=""
+          >
+            <span>
+              <Icon name="collection" />
+            </span>
+            Kitaplık
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
