6fa92d6791ee1b99eb5bc94169698a1206b87e23
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-14 15:32:58 +0300

    Sidebar component'i oluşturuldu.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,14 @@
+import Menu from "./Sidebar/Menu";
+
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="w-60 py-6 flex flex-col">
+      <a className="mb-5 px-6">
+        <img src="/src/assets/logo.svg" alt="" className="h-9" />
+      </a>
+      <Menu />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
