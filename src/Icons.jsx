78379214d9ae7752f51cbe846a0e0d2408198e86
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-20 17:54:47 +0300

    Icon eklendi.

--- src/Icons.jsx
+++ src/Icons.jsx
@@ -415,6 +415,22 @@ const LyricsIcon = ({ size }) => {
   );
 };
 
+const ArrowLeftIcon = ({ size }) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <polygon
+        points="15.54,21.151 5.095,12.229 15.54,3.309 16.19,4.069 6.635,12.229 16.19,20.39 "
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
 const Icon = ({ name, size = 24 }) => {
   const icons = {
     home: HomeIcon,
@@ -444,6 +460,7 @@ const Icon = ({ name, size = 24 }) => {
     volumeNormal: VolumeNormalIcon,
     volumeFull: VolumeFullIcon,
     lyrics: LyricsIcon,
+    arrowLeft: ArrowLeftIcon,
   };
 
   const Component = icons[name];
