4042d4bc1690bc77d118890387d629ca1b00330b
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-27 00:08:03 +0300

    Bazı kısımlar title component'ine taşındı.

--- src/components/Sidebar/SongItem.jsx
+++ src/components/Sidebar/SongItem.jsx
@@ -1,25 +1,11 @@
 /* eslint-disable react/prop-types */
-import { NavLink } from "react-router-dom";
 import Song from "../../Song";
+import Title from "../Title";
 
 function SongItem({ title, more = false, items }) {
   return (
     <section>
-      <header className="flex items-center justify-between mb-4">
-        <NavLink to={more ?? "#"}>
-          <h3 className="text-2xl text-white font-semibold tracking-tight hover:underline">
-            {title}
-          </h3>
-        </NavLink>
-        {more && (
-          <NavLink
-            className="text-xs font-semibold uppercase text-link hover:underline tracking-wider"
-            to={more}
-          >
-            SEE ALL
-          </NavLink>
-        )}
-      </header>
+      <Title title={title} more={more} />
       <div className="grid grid-cols-5 gap-x-6">
         {items.map((item) => (
           <Song item={item} key={item.id} />
