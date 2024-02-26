eed473a0afa7fd99810ff7364ae4cfbced54e9b5
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-27 00:08:26 +0300

    Navbar component'i düzenlendi.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 import Navigation from "./Navbar/Navigation";
-import { Routes, Route } from "react-router-dom";
+import { useResolvedPath } from "react-router-dom";
 import Profile from "./Navbar/Profile";
+import Search from "./Navbar/Search";
 
 function Navbar() {
+  const searchRoute = useResolvedPath("/search");
   return (
     <div className="h-[3.75rem] flex items-center justify-between px-2">
       <Navigation />
-      <Routes>
-        <Route exact path="/" element={<h2>Ana sayfa</h2>} />
-        <Route path="/search" element={<h2>Arama</h2>} />
-        <Route path="/collection" element={<h2>Kitaplık</h2>} />
-      </Routes>
+      {searchRoute && <Search />}
       <Profile />
     </div>
   );
