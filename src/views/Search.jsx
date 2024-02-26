7e5c9912f8c06ae2ffa3454c783f32ea52360287
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-27 00:07:15 +0300

    Search component'i oluşturuldu.

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,8 +1,43 @@
+/* eslint-disable react/prop-types */
+import categories from "../components/data/categories";
+import Title from "../components/Title";
+
+function Category({ category }) {
+  return (
+    <div
+      style={{ background: category.color }}
+      className="rounded-md before:pt-[100%] before:block relative"
+    >
+      <div className="absolute inset-0 overflow-hidden">
+        <h3 className="p-4 text-2xl tracking-tighter font-semibold">
+          {category.title}
+        </h3>
+        <img
+          className="w-[6.25rem] h-[6.25rem] rotate-[25deg] translate-x-[18%] translate-y-[5%] absolute bottom-0 right-0 shadow-spotify"
+          src={category.cover}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
 
 function Search() {
   return (
-    <div>Burası arama sayfasıdır.</div>
-  )
+    <>
+      <section className="mb-4">
+        <Title title="En çok dinlediğin türler" />
+      </section>
+      <section>
+        <Title title="Hepsine göz at" />
+        <div className="grid grid-cols-5 gap-6">
+          {categories.map((category, index) => (
+            <Category key={index} category={category} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Search
+export default Search;
