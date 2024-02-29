71fd6ba3c932551dc6b2b455c8a4f64add719e32
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-29 21:55:50 +0300

    CategoryItem component'i oluşturuldu.

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+
+function Category({ category }) {
+    return (
+      <div
+        style={{ background: category.color }}
+        className="rounded-md before:pt-[100%] before:block relative"
+      >
+        <div className="absolute inset-0 overflow-hidden">
+          <h3 className="p-4 text-2xl tracking-tighter font-semibold">
+            {category.title}
+          </h3>
+          <img
+            className="w-[6.25rem] h-[6.25rem] rotate-[25deg] translate-x-[18%] translate-y-[5%] absolute bottom-0 right-0 shadow-spotify"
+            src={category.cover}
+            alt=""
+          />
+        </div>
+      </div>
+    );
+  }
+
+export default Category
