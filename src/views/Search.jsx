640e471d9048f08bfb7121e9851439d9603b8069
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-29 21:56:33 +0300

    Category ve WideCategory'i component'e taşındı.

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -6,46 +6,8 @@ import favoriteCategories from "../components/data/favoriteCategories";
 import ScrollContainer from "react-indiana-drag-scroll";
 import { useEffect, useRef, useState } from "react";
 import { Icon } from "../Icons";
-
-function Category({ category }) {
-  return (
-    <div
-      style={{ background: category.color }}
-      className="rounded-md before:pt-[100%] before:block relative"
-    >
-      <div className="absolute inset-0 overflow-hidden">
-        <h3 className="p-4 text-2xl tracking-tighter font-semibold">
-          {category.title}
-        </h3>
-        <img
-          className="w-[6.25rem] h-[6.25rem] rotate-[25deg] translate-x-[18%] translate-y-[5%] absolute bottom-0 right-0 shadow-spotify"
-          src={category.cover}
-          alt=""
-        />
-      </div>
-    </div>
-  );
-}
-
-function WideCategory({ category }) {
-  return (
-    <div
-      style={{ background: category.color }}
-      className="rounded-lg flex-shrink-0 relative w-[27.375rem] h-[13.75rem]"
-    >
-      <div className="absolute inset-0 overflow-hidden">
-        <h3 className="p-4 text-[2.5rem] tracking-tighter font-semibold">
-          {category.title}
-        </h3>
-        <img
-          className="w-32 h-32 rotate-[25deg] translate-x-[18%] translate-y-[5%] absolute bottom-0 right-0 shadow-spotify"
-          src={category.cover}
-          alt=""
-        />
-      </div>
-    </div>
-  );
-}
+import Category from "~/components/CategoryItem";
+import WideCategory from "~/components/WideCategoryItem";
 
 function Search() {
   const favoritesRef = useRef();
