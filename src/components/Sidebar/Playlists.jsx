a008ba356bc367eae013536bd5422bc78cd1ae26
Zehra Şeren <fatmazehraseren@gmail.com>
2024-03-04 02:54:02 +0300

    Playlist güncellendi.

--- src/components/Sidebar/Playlists.jsx
+++ src/components/Sidebar/Playlists.jsx
@@ -1,17 +1,70 @@
 function Playlists() {
   return (
     <nav className="mx-6 mt-2 py-2 flex-auto overflow-auto border-t border-white border-opacity-20">
-      <ul>
-        {new Array(40).fill(
+      <ul>        
           <li>
             <a
               href=""
               className="text-s text-link hover:text-white flex h-8 items-center"
             >
-              22. Çalma Listem
+              Rammstein
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Jazz
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Mark Eliyahu
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Tekno
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Cover List
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Feeling Better
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Manifest
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Rap
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Rock
+            </a>
+            <a
+              href=""
+              className="text-s text-link hover:text-white flex h-8 items-center"
+            >
+              Pop
             </a>
           </li>
-        )}
+        
       </ul>
     </nav>
   );
